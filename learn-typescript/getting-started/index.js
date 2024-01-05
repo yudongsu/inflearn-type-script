762c2b183a00f46d0e43708489fe3e5bc5894d26
badongs <badongsch@gmail.com>
2024-01-05 22:55:10 +0900

    sec commit

---
+++ learn-typescript/getting-started/index.js
@@ -0,0 +1,14 @@
+// TS 튜플 - 배열의 타입까지 설정
+var address = ['강남', 10];
+// TS 객체 - 배열의 타입까지 설정
+var obj = {};
+var person = {
+    name: '이름',
+    age: 100
+};
+var person1 = {
+    name: '이름',
+    age: 1000
+};
+// TS 진위값
+var show = true;
---
+++ learn-typescript/getting-started/index.ts
@@ -0,0 +1,15 @@
+// TS 튜플 - 배열의 타입까지 설정
+let address: [string, number] = ['강남',10];
+ 
+// TS 객체 - 배열의 타입까지 설정
+let obj: object = {};
+let person: object = {
+    name:'이름',
+    age:100
+}
+let person1: {name: string, age: number } = {
+    name : '이름',
+    age: 1000
+}
+// TS 진위값
+let show: boolean = true;
