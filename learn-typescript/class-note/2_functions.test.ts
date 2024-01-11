6f1ec547b5d802c32e1c3876428ac863fb632e77
유동수 <badongsch3@gmail.com>
2024-01-11 17:58:27 +0900

    수정

---
+++ learn-typescript/class-note/2_functions.test.ts
@@ -0,0 +1,13 @@
+import { /* add the necessary imports */ } from './2_functions';
+
+describe('Test Suite', () => {
+  test('Test Case 1', () => {
+    // Add your test case here
+  });
+
+  test('Test Case 2', () => {
+    // Add your test case here
+  });
+
+  // Add more test cases as needed
+});
